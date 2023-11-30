4a2bde16860f2569387f32a40348ecdbe314dd0b
Ahhyeon-Kim <116143572+Kim-ahhyeon@users.noreply.github.com>
2023-11-30 13:24:15 +0900

    Add files via upload

---
+++ ex100_11.java
@@ -0,0 +1,14 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class ex100_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        System.out.println(b + a);
+    }
+}
---
+++ ex100_9.java
@@ -0,0 +1,13 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class ex100_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float a = sc.nextFloat();
+
+        System.out.println(a);
+    }
+}
